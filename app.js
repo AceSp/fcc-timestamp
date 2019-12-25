6a284b00c23ce986aa6b8d171804eb6778436378
Glitch (florentine-ease) <none>
2019-12-25 07:29:13 +0000

    🐬🌯 Updated with Glitch

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+app.get('/api/timestamp/', (req, res) => {
+  res.json({ unix: Date.now(), utc: Date() });
+});
+
+app.get("/api/timestamp/:date_string", (req, res) => {
+  let dateString = req.params.date_string;
+
+  if (/\d{5,}/.test(dateString)) {
+    
+   let dateInt = parseInt(dateString);
+    
+    res.json({ unix: dateString, utc: new Date(dateInt).toUTCString() });
+  }
+
+  let dateObject = new Date(dateString);
+
+  if (dateObject.toString() === "Invalid Date") {
+    res.json({ error: "Invaid Date" });
+  } else {
+    res.json({ unix: dateObject.valueOf(), utc: dateObject.toUTCString() });
+  }
+});
+
+module.exports = app;
